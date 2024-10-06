545f2e3080dc49273d5ed504babd2480fe65fb2a
zxcrowzz <pantsbro4@gmail.com>
2024-10-06 09:01:22 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,394 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const path = require("path");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require("express-flash");
+const session = require("express-session");
+const mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
+const User = require('./models/User');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const fs = require('fs');
+const https = require('https')
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+app.use(express.static(__dirname))
+const { v4: uuidV4 } = require('uuid');
+//we need a key and cert to run https
+//we generated them with mkcert
+// $ mkcert create-ca
+// $ mkcert create-cert
+const key = fs.readFileSync('cert.key');
+const cert = fs.readFileSync('cert.crt');
+
+//we changed our express setup so we can use https
+//pass the key and cert to createServer on https
+const expressServer = https.createServer({key, cert}, app);
+//create our socket.io server... it will listen to our express port
+const io = socketio(expressServer,{
+    cors: {
+        origin: [
+            "https://localhost",
+             'https://10.0.0.66' //if using a phone or another computer
+        ],
+        methods: ["GET", "POST"]
+    }
+});
+expressServer.listen(8181);
+
+//offers will contain {}
+const offers = [
+    // offererUserName
+    // offer
+    // offerIceCandidates
+    // answererUserName
+    // answer
+    // answererIceCandidates
+];
+const connectedSockets = [
+    //username, socketId
+]
+
+io.on('connection',(socket)=>{
+    // console.log("Someone has connected");
+    const userName = socket.handshake.auth.userName;
+    const password = socket.handshake.auth.password;
+
+    if(password !== "x"){
+        socket.disconnect(true);
+        return;
+    }
+    connectedSockets.push({
+        socketId: socket.id,
+        userName
+    })
+    socket.on('chat message', (message) => {
+        // Broadcast the message to all connected clients
+        io.emit('chat message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+    //a new client has joined. If there are any offers available,
+    //emit them out
+    if(offers.length){
+        socket.emit('availableOffers',offers);
+    }
+    
+    socket.on('newOffer',newOffer=>{
+        offers.push({
+            offererUserName: userName,
+            offer: newOffer,
+            offerIceCandidates: [],
+            answererUserName: null,
+            answer: null,
+            answererIceCandidates: []
+        })
+        // console.log(newOffer.sdp.slice(50))
+        //send out to all connected sockets EXCEPT the caller
+        socket.broadcast.emit('newOfferAwaiting',offers.slice(-1))
+    })
+
+    socket.on('newAnswer',(offerObj,ackFunction)=>{
+        console.log(offerObj);
+        //emit this answer (offerObj) back to CLIENT1
+        //in order to do that, we need CLIENT1's socketid
+        const socketToAnswer = connectedSockets.find(s=>s.userName === offerObj.offererUserName)
+        if(!socketToAnswer){
+            console.log("No matching socket")
+            return;
+        }
+        //we found the matching socket, so we can emit to it!
+        const socketIdToAnswer = socketToAnswer.socketId;
+        //we find the offer to update so we can emit it
+        const offerToUpdate = offers.find(o=>o.offererUserName === offerObj.offererUserName)
+        if(!offerToUpdate){
+            console.log("No OfferToUpdate")
+            return;
+        }
+        //send back to the answerer all the iceCandidates we have already collected
+        ackFunction(offerToUpdate.offerIceCandidates);
+        offerToUpdate.answer = offerObj.answer
+        offerToUpdate.answererUserName = userName
+        //socket has a .to() which allows emiting to a "room"
+        //every socket has it's own room
+        socket.to(socketIdToAnswer).emit('answerResponse',offerToUpdate)
+    })
+
+    socket.on('sendIceCandidateToSignalingServer',iceCandidateObj=>{
+        const { didIOffer, iceUserName, iceCandidate } = iceCandidateObj;
+        // console.log(iceCandidate);
+        if(didIOffer){
+            //this ice is coming from the offerer. Send to the answerer
+            const offerInOffers = offers.find(o=>o.offererUserName === iceUserName);
+            if(offerInOffers){
+                offerInOffers.offerIceCandidates.push(iceCandidate)
+                // 1. When the answerer answers, all existing ice candidates are sent
+                // 2. Any candidates that come in after the offer has been answered, will be passed through
+                if(offerInOffers.answererUserName){
+                    //pass it through to the other socket
+                    const socketToSendTo = connectedSockets.find(s=>s.userName === offerInOffers.answererUserName);
+                    if(socketToSendTo){
+                        socket.to(socketToSendTo.socketId).emit('receivedIceCandidateFromServer',iceCandidate)
+                    }else{
+                        console.log("Ice candidate recieved but could not find answere")
+                    }
+                }
+            }
+        }else{
+            //this ice is coming from the answerer. Send to the offerer
+            //pass it through to the other socket
+            const offerInOffers = offers.find(o=>o.answererUserName === iceUserName);
+            const socketToSendTo = connectedSockets.find(s=>s.userName === offerInOffers.offererUserName);
+            if(socketToSendTo){
+                socket.to(socketToSendTo.socketId).emit('receivedIceCandidateFromServer',iceCandidate)
+            }else{
+                console.log("Ice candidate recieved but could not find offerer")
+            }
+        }
+        // console.log(offers)
+    })
+
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(require('cookie-parser')());
+app.set('view engine', 'ejs');
+
+// Nodemailer transporter setup
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'pantsbro4@gmail.com', // Replace with your email
+        pass: 'tpxy ymac aupu ktow'   // Replace with your password
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+// Initialize Passport
+function initialize(passport) {
+    const authenticateUser = async (email, password, done) => {
+        try {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return done(null, false, { message: 'No user with that email' });
+            }
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password incorrect' });
+            }
+        } catch (e) {
+            return done(e);
+        }
+    };
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            done(err, null);
+        }
+    });
+}
+
+initialize(passport);
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://kingcod163:Saggytits101@cluster0.rcyom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    serverSelectionTimeoutMS: 30000
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Authentication middleware
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/home');
+    }
+    next();
+}
+
+// Register route
+app.post("/register", [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Enter a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Email already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const token = jwt.sign({ email: req.body.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        const pendingUser = new PendingUser({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            token
+        });
+
+        await pendingUser.save();
+
+        const url = `${process.env.HEROKU_APP_URL}/confirmation/${token}`;
+        await transporter.sendMail({
+            to: pendingUser.email,
+            subject: 'Confirm Email',
+            html: `Click <a href="${url}">here</a> to confirm your email.`,
+        });
+
+        res.status(201).send('User registered. Please check your email to confirm.');
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Server error');
+    }
+});
+
+// Email confirmation
+app.get('/confirmation/:token', async (req, res) => {
+    try {
+        const token = req.params.token;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const pendingUser = await PendingUser.findOne({ email: decoded.email, token });
+
+        if (!pendingUser) {
+            return res.status(400).send('Invalid token or user does not exist');
+        }
+
+        const newUser = new User({
+            name: pendingUser.username,
+            email: pendingUser.email,
+            password: pendingUser.password,
+            isConfirmed: true
+        });
+
+        await newUser.save();
+        await PendingUser.deleteOne({ email: pendingUser.email });
+
+        res.send('Email confirmed. You can now log in.');
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Server error');
+    }
+});
+
+// Login route
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    console.log("not auth")
+    res.render("login.ejs");
+});
+
+// Handle login with verification
+app.post("/login", async (req, res, next) => {
+    passport.authenticate('local', async (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect('/login');
+        }
+        req.logIn(user, async (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            // Generate a random verification code
+            const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+            // Store verification code in session
+            req.session.verificationCode = verificationCode;
+
+            // Send the verification code via email
+            await transporter.sendMail({
+                to: user.email,
+                subject: 'Your Verification Code',
+                html: `<p>Your verification code is: <strong>${verificationCode}</strong></p>`,
+            });
+
+            return res.redirect('/verify');
+        });
+    })(req, res, next);
+});
+
+// Verification route
+app.get('/verify', (req, res) => {
+    res.render('verify.ejs');
+});
+
+// Handle verification code submission
+app.post('/verify', (req, res) => {
+    const { code } = req.body;
+
+    if (code === req.session.verificationCode) {
+        const roomId = uuidV4(); 
+        return res.redirect('/index.html');
+    } else {
+        res.send('Invalid verification code. Please try again.');
+    }
+});
+app.get('/index.html', checkAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+// Redirect root to a new room
+app.get('/', (req, res) => {
+    const roomId = uuidV4();
+    if (req.isAuthenticated()) {
+        res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Room route
+app.get('/:index.html:', (req, res) => {
+    res.render('index', { roomId: req.params.room });
+});
+
+// Registration route
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render("register.ejs");
+});
