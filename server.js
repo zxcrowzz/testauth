9e21cddf4226aded1351f60a41463ccf8af9ed80
zxcrowzz <pantsbro4@gmail.com>
2024-10-06 21:17:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ expressServer.listen(process.env.PORT || 8181, () => {
 const io = socketio(expressServer,{
     cors: {
         origin: [
-            "https://10.0.0.66",
-             'https://r3dxx-9ce6f110c87b.herokuapp.com' //if using a phone or another computer
+            'https://10.0.0.66',
+             "https://r3dxx-9ce6f110c87b.herokuapp.com" //if using a phone or another computer
         ],
         methods: ["GET", "POST"]
     }
