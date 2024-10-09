4fe30dcbec033ad7401fc882ef614aee39c06082
zxcrowzz <pantsbro4@gmail.com>
2024-10-08 22:43:45 -0700

    Update socketListeners.js

--- socketListeners.js
+++ socketListeners.js
@@ -25,7 +25,7 @@ function createOfferEls(offers){
     offers.forEach(o=>{
         console.log(o);
         const newOfferEl = document.createElement('div');
-        newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`
+        newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`;
         newOfferEl.addEventListener('click',()=>answerOffer(o))
         answerEl.appendChild(newOfferEl);
     })
