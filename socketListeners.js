aad82625bb4da40a48991bc4593a8810771b4555
zxcrowzz <pantsbro4@gmail.com>
2024-10-08 21:43:09 -0700

    Update socketListeners.js

--- socketListeners.js
+++ socketListeners.js
@@ -1,35 +1,48 @@
 
 //on connection get all available offers and call createOfferEls
-socket.on('availableOffers',offers=>{
-    console.log(offers)
-    createOfferEls(offers)
-})
+let currentRoom = null;
 
-//someone just made a new offer and we're already here - call createOfferEls
-socket.on('newOfferAwaiting',offers=>{
-    createOfferEls(offers)
-})
+// When connected, get all available offers for the current room
+socket.on('availableOffers', (offers) => {
+    console.log(offers);
+    if (currentRoom) {
+        createOfferEls(offers.filter(o => o.room === currentRoom));
+    }
+});
 
-socket.on('answerResponse',offerObj=>{
-    console.log(offerObj)
-    addAnswer(offerObj)
-})
+// Someone just made a new offer and we're already in the room
+socket.on('newOfferAwaiting', (offers) => {
+    if (currentRoom) {
+        createOfferEls(offers.filter(o => o.room === currentRoom));
+    }
+});
 
-socket.on('receivedIceCandidateFromServer',iceCandidate=>{
-    addNewIceCandidate(iceCandidate)
-    console.log(iceCandidate)
-})
+// Listen for answer responses
+socket.on('answerResponse', (offerObj) => {
+    if (offerObj.room === currentRoom) {
+        console.log(offerObj);
+        addAnswer(offerObj);
+    }
+});
 
-function createOfferEls(offers){
-    //make green answer button for this new offer
+// Listen for ICE candidates from the server
+socket.on('receivedIceCandidateFromServer', (iceCandidate) => {
+    addNewIceCandidate(iceCandidate);
+    console.log(iceCandidate);
+});
+
+// Create offer elements for the available offers
+function createOfferEls(offers) {
+    clearAnswerButtons(); // Clear old buttons before adding new ones
     const answerEl = document.querySelector('#answer');
-    offers.forEach(o=>{
+    
+    offers.forEach(o => {
         console.log(o);
         const newOfferEl = document.createElement('div');
-        newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`
-        newOfferEl.addEventListener('click',()=>answerOffer(o))
+        newOfferEl.innerHTML = `<button class="btn btn-success col-1">Answer ${o.offererUserName}</button>`;
+        newOfferEl.addEventListener('click', () => answerOffer(o));
         answerEl.appendChild(newOfferEl);
-    })
+    });
 }
 const callButton = document.querySelector('#Hangup'); // Select the button by its ID
 callButton.disabled = true;
